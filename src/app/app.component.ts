3046fdcd76253e33e206aff97b084c9c7d77044d
Tinnakorn Choompee <tinnakornchoompee@gmail.com>
2019-07-28 23:53:57 +0700

    Finish Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { Com1Component } from './components/com1/com1.component';
+import { Com2Component } from './components/com2/com2.component';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'component';
+  title = 'Angular Component';
+  count = 0;
+
+  @ViewChild("c1", {static: true}) c1:Com1Component;
+  @ViewChild("c2", {static: true}) c2:Com2Component;
+
+  onClickCount() {
+    this.count = this.count + 1;
+  }
+
+  onComReset(value) {
+    this.count = value;
+  }
+
+  onClickRun3() {
+    this.c1.count = 99;
+    this.c2.count = 11;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Com1Component } from './components/com1/com1.component';
+import { Com2Component } from './components/com2/com2.component';
+import { ShareService } from './services/share.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Com1Component,
+    Com2Component
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [ShareService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
